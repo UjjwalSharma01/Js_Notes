52518b0b2d1f873affbadca45c9499a5a8e6db55
UjjwalSharma01 <ujjwalsh2004@gmail.com>
2024-03-29 13:50:12 +0530

    Add some changes in variable.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
-const accountId = 144553
+const accountId = 144553 // cannot be changes later
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur" // not the preffered way to use variables like this
 let accountState;
 
 // accountId = 2 // not allowed
@@ -16,7 +16,11 @@ console.log(accountId);
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
+
+var is having global scope whereas let is having local scope
 */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // when you want to print the value of multiple variables at once
+
+console.table([accountPassword, accountCity]);
