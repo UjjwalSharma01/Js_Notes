cae07e628f26f3b7734043b1210904c9f7229693
Ujjwal Sharma <113621084+UjjwalSharma01@users.noreply.github.com>
2024-03-31 05:34:24 +0000

    Refactor array manipulation and add new methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,14 @@
 // array
 
+/*
+
+arrays arrays are resizable in JavaScript, and elements can be added and removed.
+
+values are accessed using the index, which starts from 0.
+copy operations always create shallow copies which means that the original array is effected if the copied array is changed.
+
+*/
+
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
@@ -7,24 +16,38 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
 // Array methods
+/*
+Methods are as follows:
+1. push => adds an element to the end of the array
+2. pop => removes an element from the end of the array
+3. unshift => adds an element to the beginning of the array
+4. shift => removes an element from the beginning of the array
+5. includes => checks if an element is present in the array, returns a boolean
+6. indexOf => returns the index of the element, if not present returns -1
+7. join => joins the elements of the array, returns a string
+8. slice => returns a shallow copy of the array, means original array will be effected if the copied array is changed
+9. splice => removes elements from the array and returns the removed elements
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+*/
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+console.log(myArr);
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+myArr.unshift(9)
+myArr.shift() // removes the first element
 
-// const newArr = myArr.join()
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
 
-// console.log(myArr);
-// console.log( newArr);
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log( newArr);
 
 
-// slice, splice
+// slice, splice => very important in interview
 
 console.log("A ", myArr);
 
@@ -37,3 +60,11 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
+
+/*
+Difference between slice and splice
+very very very importantant
+1. slice => returns a shallow copy of the array, means original array will be effected if the copied array is changed, it manipulates the original array
+2. splice => removes elements from the array and returns the removed elements
+
+*/
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,31 +1,36 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros) // should be ["thor", "Ironman", "spiderman", ["superman", "flash", "batman"]] array ke andar array and second array is treated as a single element
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
+/* cancat will return the result in the new array */
+const allHeros = marvel_heros.concat(dc_heros) // this will not change the original array
+console.log(allHeros);
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
 
+// spread operator => ek kaach ka glass liya drop krdia it will spread
+// this is most used in react and in general in JS
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]] // nested array
 
-const real_another_array = another_array.flat(Infinity)
+// flat method => it will flatten the array,means it will remove the nested array, returns a new array and does not change the original array
+const real_another_array = another_array.flat(Infinity) // inifiity is used to remove all the nested arrays, othwerwise we can pass 1, 2, 3 in depth to remove the nested arrays
 console.log(real_another_array);
 
+// 
+console.log(Array.isArray("Hitesh")) // false
+console.log(Array.from("Hitesh")) // ["H", "i", "t", "e", "s", "h"] => converts the string into an array or any iterable object into an array
 
-
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// asked in interviews
+console.log(Array.from({name: "hitesh"})) // [] => empty array as it is not an iterable object, it should be an array or a string or a set or a map, you need explicitly mention ki keys se array banana hai ya values se
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); // [100, 200, 300] => it will create an array of the values passed to it
