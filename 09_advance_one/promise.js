a701d0da5f4b4ad6ea6472bb4f03373036f75ac7
Ujjwal Sharma <113621084+UjjwalSharma01@users.noreply.github.com>
2024-04-22 17:03:27 +0000

    did come changes

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -1,1 +1,10 @@
 console.log("promises");
+
+const promise1 = new Promise(function(resolve,reject){
+    console.log("promise1 executed");
+    resolve();
+})
+
+promise1.then(()=>{
+    console.log("promise1 resolved");
+})
